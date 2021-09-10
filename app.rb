54bbfbf5aa7f7916b2c7fc137c2ba0881e648d1a
Eugene Mazgalev <e.mazgalev@gmail.com>
2021-09-10 15:53:39 +0300

    add worked mongo

--- app.rb
+++ app.rb
@@ -4,11 +4,29 @@ require 'telegram/bot'
 
 token = 'token'
 
-Telegram::Bot::Client.run(token) do |bot|
-  bot.listen do |message|
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-    end
-  end
+puts 'Start'
+# Telegram::Bot::Client.run(token) do |bot|
+  # bot.listen do |message|
+    # case message.text
+    # when '/start'
+      # bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    # end
+  # end
+# end
+
+require 'mongo_mapper'
+
+MongoMapper.connection = Mongo::Client.new('mongodb://mongo:27017')
+
+class User
+  include ::MongoMapper::Document
+
+  key :name, String
+  key :age,  Integer
+
 end
+user = User.new(:name => 'Brandon')
+user.save!
+
+puts User.where(:name => 'Brandon').first
+puts 'Success'
